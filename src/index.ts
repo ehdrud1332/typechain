4a1429635712e45c57133353ac28ae6d27fb65b5
castle.jaemin <ehdrud1332@gmail.com>
2021-03-13 11:01:35 +0900

    Conclusions

--- src/index.ts
+++ src/index.ts
@@ -76,6 +76,7 @@ const createNewBlock = (data: string): Block => {
     data,
     newtTimestamp
   );
+  addBlock(newBlock);
   return newBlock;
 };
 
@@ -108,5 +109,11 @@ const addBlock = (candidateBlock: Block): void => {
     blockchain.push(candidateBlock);
   }
 };
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
